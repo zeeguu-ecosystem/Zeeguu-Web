0477759ae16451602daf2c490a0d0a83b862340a
Mircea Lungu <mircea.lungu@gmail.com>
2017-03-05 20:15:00 +0100

    removed useless comment

--- zeeguu_web/gym/__init__.py
+++ zeeguu_web/gym/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf8 -*-
 import flask
 # We need to define the blueprint here, because all the files containing controllers use it
-print ("--->>> the gym module will be named..." + __name__)
 gym = flask.Blueprint("gym", __name__)
 import views
 
