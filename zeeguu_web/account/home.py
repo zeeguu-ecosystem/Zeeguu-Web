fe71ce089f39685d5cce447024f10317da104152
Mircea Lungu <mircea.lungu@gmail.com>
2017-04-02 16:35:02 +0200

    added favicon

--- zeeguu_web/account/home.py
+++ zeeguu_web/account/home.py
@@ -1,5 +1,10 @@
+import os
+
+from flask import send_from_directory
+
 from . import account
 import flask
+import zeeguu
 
 
 @account.route("/")
@@ -7,3 +12,10 @@ def home():
     if "user" in flask.session:
         return flask.redirect(flask.url_for("account.bookmarks"))
     return flask.render_template("index.html")
+
+
+@account.route("/favicon.ico")
+def get_favicon():
+    return send_from_directory(os.path.join(zeeguu.app.root_path, 'static'), 'img/favicon.ico')
+
+
