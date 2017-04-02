c92bbc45d80616af878f6f2aab1657379a8d0440
Mircea Lungu <mircea.lungu@gmail.com>
2017-04-02 16:35:33 +0200

    added reference

--- zeeguu_web/account/home.py
+++ zeeguu_web/account/home.py
@@ -14,6 +14,7 @@ def home():
     return flask.render_template("index.html")
 
 
+# cf. http://flask.pocoo.org/docs/0.12/patterns/favicon/
 @account.route("/favicon.ico")
 def get_favicon():
     return send_from_directory(os.path.join(zeeguu.app.root_path, 'static'), 'img/favicon.ico')
