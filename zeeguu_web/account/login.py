ce6f9808677dc2fb710f417ec4c4da284f18509f
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-30 20:00:31 +0200

    a bit of refactoring

--- zeeguu_web/account/login.py
+++ zeeguu_web/account/login.py
@@ -5,6 +5,17 @@ from . import account, login_first
 import flask
 from zeeguu.model import User, Session
 
+from flask import flash
+from zeeguu.model.language import Language
+import sqlalchemy.exc
+
+
+KEY_USER_ID = "user_id"
+KEY_USER_NAME = "user_name"
+KEY_API_SESSION_ID = "api_session_id"
+
+SESSION_KEYS = [KEY_USER_ID, KEY_USER_NAME, KEY_API_SESSION_ID]
+
 
 @account.route("/login", methods=("GET", "POST"))
 def login():
@@ -12,11 +23,10 @@ def login():
     
         check user credentials 
         
-        if it contains the ?next=... parameter, it will redirect
-        to there on success 
+        if next=... parameter is present redirect on success
         
-    :return: 
     """
+
     form = flask.request.form
     if flask.request.method == "POST" and form.get("login", False):
         password = form.get("password", None)
@@ -28,14 +38,101 @@ def login():
             if user is None:
                 flask.flash("Invalid email and password combination")
             else:
-                flask.session["user_id"] = user.id
-                flask.session["name"] = user.name
-                flask.session.permanent = True
-
-                response = make_response(redirect(flask.request.args.get("next") or flask.url_for("account.bookmarks")))
-                return response
+                _set_session_data(user)
+                return make_response(redirect(flask.request.args.get("next") or flask.url_for("account.bookmarks")))
 
     return flask.render_template("login.html")
+
+
+@account.route("/create_account", methods=("GET", "POST"))
+def create_account():
+
+    # A cool way of passing the arguments to the flask template
+    template_arguments = dict (
+         languages= Language.all(),
+         native_languages = Language.native_languages(),
+         default_learned= Language.default_learned()
+    )
+
+    # GET
+    if flask.request.method == "GET":
+        return flask.render_template("create_account.html", **template_arguments)
+
+    # POST
+    form = flask.request.form
+    password = form.get("password", None)
+    email = form.get("email", None)
+    name = form.get("name", None)
+    code = form.get("code", None)
+    language = Language.find(form.get("language", None))
+    native_language = Language.find(form.get("native_language", None))
+
+    if not code in zeeguu.app.config.get("INVITATION_CODES"):
+        flash("Invitation code is not recognized. Please contact us.")
+
+    elif password is None or email is None or name is None:
+        flash("Please enter your name, email address, and password")
+
+    else:
+        try:
+
+            zeeguu.db.session.add(User(email, name, password, language, native_language))
+            zeeguu.db.session.commit()
+
+            user = User.authorize(email, password)
+
+            _set_session_data(user)
+
+            return flask.redirect(flask.url_for("account.my_account"))
+
+        except ValueError:
+            flash("Username could not be created. Please contact us.")
+        except sqlalchemy.exc.IntegrityError:
+            flash(email + " is already in use. Please select a different email.")
+        except:
+            flash("Something went wrong. Please contact us.")
+        finally:
+            zeeguu.db.session.rollback()
+
+    return flask.render_template("create_account.html", **template_arguments)
+
+
+@account.route("/logout")
+@login_first
+def logout():
+
+    for key in SESSION_KEYS:
+        flask.session.pop(key, None)
+
+    return make_response(redirect(flask.url_for("account.home")))
+
+
+@account.route("/logged_in")
+@login_first
+def logged_in():
+    if flask.session.get("user_id", None):
+        return "YES"
+    return "NO"
+
+
+def _set_session_data(user: User):
+    """
+    
+        extracted to its own function, since it's duplicated between login and create_account 
+        
+    """
+
+    api_session = Session.find_for_user(user)
+    zeeguu.db.session.add(api_session.id)
+    zeeguu.db.session.commit()
+
+    flask.session[KEY_USER_ID] = user.id
+    flask.session[KEY_USER_NAME] = user.name
+    flask.session[KEY_API_SESSION_ID] = api_session.id
+
+    flask.session.permanent = True
+
+
 
 
 # @account.route("/login_with_session", methods=["POST"])
@@ -64,24 +161,3 @@ def login():
 #         return "FAIL"
 #
 #     return "OK"
-
-
-@account.route("/logout")
-@login_first
-def logout():
-    # Note, that there is also an API endpoint for logout called logout_session
-    flask.session.pop("user_id", None)
-    flask.session.pop("name", None)
-
-    response = make_response(redirect(flask.url_for("account.home")))
-    # response.set_cookie('sessionID', '', expires=0)
-
-    return response
-
-
-@account.route("/logged_in")
-@login_first
-def logged_in():
-    if flask.session.get("user_id", None):
-        return "YES"
-    return "NO"
