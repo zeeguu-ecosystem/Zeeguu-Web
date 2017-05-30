d438ab8365c9d85d2a8084fcdd7088320c19d235
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-30 20:44:24 +0200

    .

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setuptools.setup(
     description="Basic Web Presence for Zeeguu",
     keywords="Zeeguu is an ecosystem which aims at accelerating second language acquisition",
     dependency_links=[
-        "git+https://github.com/mircealungu/zeeguu-core.git#egg=zeeguu"
-        "git+https://github.com/mircealungu/Unified-Multilanguage-Reader.git#egg=umr"
+        "git+https://github.com/mircealungu/zeeguu-core.git#egg=zeeguu",
+        "git+https://github.com/mircealungu/Unified-Multilanguage-Reader.git#egg=umr",
         "git+https://github.com/mircealungu/practice-as-a-service.git#egg=zeeguu_exercises"
     ],
     install_requires=("flask>=0.10.1",
