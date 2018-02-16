d8a61a05ac9f530999940ffbedf8f7046a056818
Lars Holdijk <larsholdijk@gmail.com>
2018-02-16 23:01:43 +0100

    Added nltk to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-
+import nltk
 import setuptools
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
+class DevelopScript(develop):
+    def run(self):
+        develop.run(self)
+        ntlk_install_packages()
+
+
+class InstallScript(install):
+    def run(self):
+        install.run(self)
+        ntlk_install_packages()
+
+
+def ntlk_install_packages():
+    print("Downloading nltk packages...")
+    nltk.download('punkt')
 
 setuptools.setup(
     name="zeeguu_web",
@@ -14,6 +31,10 @@ setuptools.setup(
     author_email="me@mir.lu",
     description="Basic Web Presence for Zeeguu",
     keywords="Zeeguu is an ecosystem which aims at accelerating second language acquisition",
+    cmdclass={
+        'develop': DevelopScript,
+        'install': InstallScript,
+    },
     dependency_links=[
         "git+https://github.com/mircealungu/zeeguu-core.git#egg=zeeguu",
         "git+https://github.com/mircealungu/Unified-Multilanguage-Reader.git#egg=umr",
