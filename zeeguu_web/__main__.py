e82f686221ccec8312cd98ae7f52075b8c7d11aa
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-13 16:53:02 +0100

    cleaned up the default_web.cfg

--- zeeguu_web/__main__.py
+++ zeeguu_web/__main__.py
@@ -11,5 +11,5 @@ application = app.app
 print("Instance folder:", application.instance_path)
 application.run(
     host=application.config.get("HOST", "localhost"),
-    port=application.config.get("PORT", 9002)
+    port=application.config.get("PORT", 9000)
 )            
