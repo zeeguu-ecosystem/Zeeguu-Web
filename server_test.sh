933295d463c1f47ef80fc9e1eee68c5a0a950862
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-13 16:55:24 +0100

    ..

--- server_test.sh
+++ server_test.sh
@@ -1,3 +1,3 @@
 # set ZEEGUU_WEB_CONFIG in your .bashrc if you don't want to go with the default
-[ -z $ZEEGUU_WEB_CONFIG ] && export ZEEGUU_WEB_CONFIG=`pwd`/default_web.cfg
+[ -z $ZEEGUU_WEB_CONFIG ] && export ZEEGUU_WEB_CONFIG=`pwd`/default_web.cfg 
 python -m zeeguu_web
