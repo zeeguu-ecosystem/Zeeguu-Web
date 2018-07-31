256b24d523149aa98da9e305fde116ecb5948cb0
Mircea Lungu <mircea.lungu@gmail.com>
2018-07-31 20:02:22 +0200

    fixed favicon.ico

--- zeeguu_web/account/home.py
+++ zeeguu_web/account/home.py
@@ -1,19 +1,15 @@
-import os
-
-from flask import send_from_directory
+from flask import url_for
 
 from zeeguu_web.account.api.teacher import is_teacher
 from zeeguu_web.constants import KEY__SESSION_ID
 
 from . import account, login_first
 import flask
-import zeeguu
 
 
-# cf. http://flask.pocoo.org/docs/0.12/patterns/favicon/
 @account.route("/favicon.ico")
 def get_favicon():
-    return send_from_directory(os.path.join(zeeguu.app.root_path, 'static'), 'img/favicon.ico')
+    return flask.redirect(url_for('static', filename='img/favicon.ico'))
 
 
 @account.route("/")
