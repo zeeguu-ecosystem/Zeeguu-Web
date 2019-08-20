2b3813a4b60d9d624a30c619cd0421d1d5be7288
Mircea Lungu <mircea.lungu@gmail.com>
2019-08-20 18:01:47 +0200

    conference pages

--- zeeguu_web/static_pages/conferences.py
+++ zeeguu_web/static_pages/conferences.py
@@ -6,3 +6,8 @@ import flask
 @static_pages.route("/easeai")
 def easeai():
     return flask.render_template("static_pages/easeai.html")
+
+
+@static_pages.route("/eurocall")
+def eurocall():
+    return flask.render_template("static_pages/eurocall.html")
