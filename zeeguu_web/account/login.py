8d58e58b0913fbd7a0c13d815e8681391c29adac
kapathy <katrineiv@hotmail.com>
2020-04-24 12:42:14 +0200

    login redirects to articles

--- zeeguu_web/account/login.py
+++ zeeguu_web/account/login.py
@@ -36,7 +36,7 @@ def login():
                 flask.flash(e.message)
 
             else:
-                response = make_response(redirect(flask.request.args.get("next") or flask.url_for("account.whatnext")))
+                response = make_response(redirect(flask.request.args.get("next") or flask.url_for("account/read")))
 
                 set_session_data(details, sessionID, response)
 
